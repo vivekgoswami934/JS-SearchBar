9e369f6000b5acc84233f10b3d0f0d2e6ca1f66a
Vivek Goswami <101577915+vivekgoswami934@users.noreply.github.com>
2022-10-12 08:36:23 +0530

    done final commit

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 let characterList = document.getElementById("characterList");
 const URL = `https://hp-api.herokuapp.com/api/characters`;
-
 let datas = [];
+
+let searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+
+  const searchString = e.target.value.toLowerCase();
+   console.log(datas)
+  const filterData = datas.filter((character) => {
+    return  character.name.toLowerCase().includes(searchString) || character.house.toLowerCase().includes(searchString);
+  });
+
+  console.log(filterData);
+
+  display(filterData)
+});
 
 const getData = async () => {
   try {
     const res = await fetch(URL);
     result = await res.json();
-    datas = result.splice(0, 26);
+    datas = result.splice(0, 24);
     display(datas);
     // console.log(datas.splice(50, 20));
   } catch (error) {
